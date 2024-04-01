28cdc9606a317fa509707e376ef7b4a1937f6c23
Aloiszz <a.tupinon@rubika-edu.com>
2024-04-01 13:08:24 +0200

    base multiplayer

---
+++ RussianRoulette/Assets/Script/UI/ConsoleUI.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Linq;
+using TMPro;
+using UnityEngine;
+
+public class ConsoleUI : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI text;
+    [SerializeField] int             maxLineCount = 10;
+
+    string myLog;
+    int    lineCount;
+
+    void OnEnable()
+    {
+        Application.logMessageReceived += Log;
+    }
+
+    void OnDisable()
+    {
+        Application.logMessageReceived -= Log;
+    }
+
+    private void Log(string logString, string stackTrace, LogType type)
+    {
+        switch (type)
+        {
+            case LogType.Assert:
+                logString = "<color=white>" + logString + "</color>";
+                break;
+            case LogType.Log:
+                logString = "<color=white>" + logString + "</color>";
+                break;
+            case LogType.Warning:
+                logString = "<color=yellow>" + logString + "</color>";
+                break;
+            case LogType.Exception:
+                logString = "<color=red>" + logString + "</color>";
+                break;
+            case LogType.Error:
+                logString = "<color=red>" + logString + "</color>";
+                break;
+        }
+
+        myLog = myLog + "\n" + logString;
+        lineCount++;
+
+        if (lineCount > maxLineCount)
+        {
+            lineCount--;
+            // myLog = myLog.Substring(myLog.IndexOf("\n") + 1);
+            myLog = DeleteLines(myLog, 1);
+        }
+
+        text.text = myLog;
+    }
+
+    string DeleteLines(string message, int linesToRemove)
+    {
+        return message.Split(Environment.NewLine.ToCharArray(), linesToRemove + 1).Skip(linesToRemove).FirstOrDefault();
+    }
+}
---
+++ RussianRoulette/Assets/Script/UI/NetworkManagerUI.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Net.NetworkInformation;
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkManagerUI : MonoBehaviour
+{
+    public Button btn_Host;
+    public Button btn_Server;
+    public Button btn_Client;
+
+
+    private void Awake()
+    {
+        btn_Host.onClick.AddListener((() => NetworkManager.Singleton.StartHost()));
+        btn_Server.onClick.AddListener((() => NetworkManager.Singleton.StartServer()));
+        btn_Client.onClick.AddListener((() => NetworkManager.Singleton.StartClient()));
+    }
+}
